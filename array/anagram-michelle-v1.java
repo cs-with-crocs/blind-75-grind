9d86c65014448ba10d44145f7ecb53d83216cfd1
Michelle <mwang45@student.ubc.ca>
2022-09-07 22:55:50 -0700

    Added runtime and space notes

--- array/anagram-michelle-v1.java
+++ array/anagram-michelle-v1.java
@@ -9,3 +9,8 @@ class Solution {
         return Arrays.equals(sChar, tChar);
     }
 }
+
+/*
+Runtime: 9 ms, faster than 39.14% of Java online submissions for Valid Anagram.
+Memory Usage: 44.7 MB, less than 40.58% of Java online submissions for Valid Anagram. 
+*/
